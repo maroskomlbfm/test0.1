befaf4f949f302582781c0f7823573d973987592
maroskomlbfm <44090230+maroskomlbfm@users.noreply.github.com>
2018-10-19 15:06:24 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+var a =0;
+var b = 0;
+var c = 0;
+var i = 1;
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(400,400);
 }
 
 function draw() {
-  ellipse(mouseX, mouseY, 80, 80);
+	
+	background(a,b,c)
+	a = a  + i;
+	
+	if(a>255)  {
+		i = -1;
+	}
+	
+	
+	if(a< 0){
+		i - 1;
+	}
+
 }
